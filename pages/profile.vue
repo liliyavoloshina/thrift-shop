7e50a57c0b35d04f5086322c4a4c1b8f13a80d38
Liliya <liliyavoloshine@gmail.com>
2021-04-28 18:01:10 +0400

    profile style

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
-    <h2>Profile</h2>
-    <p>{{user}}</p>
-    <button @click="logout" class="button">Logout</button>
+    <p> <span class="label">Name:</span> {{user.name}}</p>
+    <p> <span class="label">Location:</span> {{user.location}}</p>
+    <p><span class="label">Rating:</span> {{user.rating}}</p>
+    <p><span class="label">Reviews:</span> {{user.reviews}}</p>
+    <p><span class="label">Email:</span> <a :href="`mailto:${user.email}`" class="email-link">{{user.email}}</a></p>
+    <button @click="logout" class="button button-logout">Logout</button>
   </div>
 </template>
 
@@ -23,4 +26,27 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+p {
+  font-size: 2rem;
+  .label {
+    font-weight: 600;
+  }
+  .email-link {
+    color: $accent;
+    text-decoration: underline;
+    &:hover {
+      color: $accent-2;
+    }
+  }
+}
+.button-logout {
+  background-color: $error-message;
+  color: white;
+  width: 200px;
+  height: 50px;
+  font-size: 1.5rem;
+  &:hover {
+    opacity: 0.4;
+  }
+}
 </style>
