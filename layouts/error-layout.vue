7d13239b9d34ee7d09ffa09799202dfa25d430ae
Liliya <liliyavoloshine@gmail.com>
2021-05-11 12:50:28 +0400

    added error page

---
+++ layouts/error-layout.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="error-wrapper">
+    <Nuxt/>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.error-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  background-color: $accent;
+}
+</style>
